fe560ca23bafaea5185cf085d65abf498a0f4d02
Isaac Nankavill <me@isaacnankavill.com>
2017-04-03 20:33:42 +1000

    Junior coaching content

--- junior-coaching.php
+++ junior-coaching.php
@@ -1,7 +1,7 @@
 <?php include('head.php');?>
 		<div class="main-container">
 					
-			<section class="image-square right">
+			<section class="image-square right" style='max-height: 840px;'>
 		        <div class="col-md-6 image">
 		            <div class="background-image-holder">
 		                <img alt="image" class="background-image" src="img/scorpion-junior-side.jpeg">
@@ -10,11 +10,12 @@
 		        <div class="col-md-6 content">
 		            <h3>Junior Coaching</h3>
 		            <p class="mb0">
-		                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+		                The aim of our academy is to provide the best tennis teaching facility for junior players so that they can develop their skills to an advanced level as they get older. THerefore, we have a comprehensive range of classes tailored for all levels and ages at four different schools from Monday to Saturday during school term (subject to public holidays).
 		            </p><br> <br><h3>Fixtures Competition</h3><p class="mb0">
-		                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+		                The Scorpion International Tennis' Fixtures Competition program is designed to provide your child the opportunity to enjoy useful match play experience with the other kids with the same skill level.
+		                Fixtures are held each week of the school term on Saturday mornings at Centenery SHS (subject to public holidays).
 		            </p><br> <br><h3>Squad Training Program</h3><p class="mb0">
-		                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+		                Our squad training program is designed based on the Spanish tennis intensity training. Students in these classes will have an opportunity to excel in their high level of competency. The program will equip students to participate in tournament programs, both state-wide and nationally. Once they get to the proficient level, students can have access to our Summer Vacation International program in major tennis cities such as Los Angeles, Spain and Bangkok. These programs will assist them to learn important tactics and tournament work, enabling them to enter the professional level.
 		            </p>
 		        </div>
 		    </section>
@@ -24,7 +25,7 @@
 		            <div class="row">
 		                <div class="col-sm-12 text-center">
 		                    <h4 class="uppercase mb16">Junior Private Coaching</h4>
-		                    <p class="lead mb64">Lorem ipsum dolor sit amet.&nbsp;Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.  </p>
+		                    <p class="lead mb64">Scorpion's private coaching offers the most effective way to boost juniors' tennis skills through personalised one-on-one sessions with our highly qualified coaches. Semi private lessons (shared private lessons) are also available ($5 extra) (2 person limit)</p>
 		                </div>
 		            </div>
 		            
