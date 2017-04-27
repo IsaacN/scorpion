b86feb87f14d3fa1d63c228d615b994ceb60cff9
Isaac Nankavill <me@isaacnankavill.com>
2017-04-27 12:45:39 +1000

    Centre Images

--- centres.php
+++ centres.php
@@ -26,13 +26,15 @@
 		        <div class="container">
 		            <div class="row">
 		                <div class="col-sm-6">
+		                	<div class='col-sm-10 col-sm-offset-1'><img src='img/scorpion-parish.jpg'></div>
 		                    <h2 class="number">Jindalee Parish Centre</h2>
 		                    <h4>109 Yallambee Rd, Jindalee QLD 4074</h4>
 		                    <p>
 		                        Full-time operating commercial center for Scorpion Hotshots and Adult Coaching programs. Available all year long.
-		                    </p>
+		                    </p> 
 		                </div>
 		                <div class="col-sm-6">
+		                	<div class='col-sm-10 col-sm-offset-1'><img src='img/scorpion-centenery.jpg'></div>
 		                    <h2 class="number">Centenary State High School</h2>
 		                    <h4>22 Curragundi Rd, Jindalee QLD 4074</h4>
 		                    <p>
@@ -47,21 +49,17 @@
 		    </section><section>
 		        <div class="container">
 		            <div class="row">
-		                <div class="col-sm-4">
-		                    <h2 class="number">Karalee State School</h2>
-		                    <h4>Arthur Summervilles Rd, Karalee QLD 4306</h4>
-		                    <p>
-		                        Classes in Karalee State School usually run on Thursday and Friday afternoons during all school terms. 
-		                    </p>
-		                </div>
-		                <div class="col-sm-4">
+		                
+		                <div class="col-sm-6">
+		                	<div class='col-sm-10 col-sm-offset-1'><img src='img/scorpion-kenmore.jpg'></div>
 		                    <h2 class="number">Kenmore State High School</h2>
 		                    <h4>20 Paley St, Kenmore Hills QLD 4069</h4>
 		                    <p>
 		                        Kenmore State High School offers state-of-the-art facilities with six courts. It is privately managed, perfect training facility for tournament level. Three different types of hard court surfaces allow juniors to gain experience on fast and slow hard courts. 
 		                    </p>
 		                </div>
-		                <div class="col-sm-4">
+		                <div class="col-sm-6">
+		                <div class='col-sm-10 col-sm-offset-1'><img src='img/scorpion-mt-crosby.jpg'></div>
 		                    <h2>Mount Crosby State School</h2>
 		                    <h4>Mount Crosby Rd, Mount Crosby QLD​ 4306</h4>
 		                    <p>
@@ -69,7 +67,16 @@
 		                    </p>
 		                </div>
 		            </div>
-		            
+		            <div class='row'>
+		            <div class="col-sm-6 col-sm-offset-3">
+
+		                    <h2 class="number">Karalee State School</h2>
+		                    <h4>Arthur Summervilles Rd, Karalee QLD 4306</h4>
+		                    <p>
+		                        Classes in Karalee State School usually run on Thursday and Friday afternoons during all school terms. 
+		                    </p>
+		                </div>
+		            </div>
 		        </div>
 		        
 		    </section>			
